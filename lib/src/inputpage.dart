87c6f40da9794dba99fa3dde0105278043ae9c62
Charlie Reign <charlesgold45@gmail.com>
2021-08-21 10:25:09 -0700

    imported previous code

--- lib/src/inputpage.dart
+++ lib/src/inputpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const bottomContainerColour = 0xFFEB1555;
@@ -20,30 +21,34 @@ class _InputPageState extends State<InputPage> {
           child: Row(
             children: [
               Expanded(
-                child: ReusableCard(Color(cardColour)),
+                child: ReusableCard(
+                  Color(cardColour),
+                  IconContent(FontAwesomeIcons.mars, 'MALE'),
+                ),
               ),
               Expanded(
-                child: ReusableCard(Color(cardColour)),
+                child: ReusableCard(
+                  Color(cardColour),
+                  IconContent(FontAwesomeIcons.venus, 'FEMALE'),
+                ),
               ),
             ],
           ),
         ),
         Expanded(
-          child: ReusableCard(Color(cardColour)),
+          child: ReusableCard(Color(cardColour),Text('Middle')),
         ),
         Expanded(
             child: Row(
-          children: [
-            Expanded(
-              child: ReusableCard(Color(cardColour)),
-            ),
-            Expanded(
-              child: ReusableCard(
-                Color(cardColour),
-              ),
-            )
-          ],
-        )),
+              children: [
+                Expanded(
+                  child: ReusableCard(Color(cardColour),Text('Icon')),
+                ),
+                Expanded(
+                  child: ReusableCard(Color(cardColour),Text('Icon')),
+                )
+              ],
+            )),
         Container(
           color: Color(bottomContainerColour),
           margin: EdgeInsets.all(10.0),
@@ -55,13 +60,44 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String gender;
+
+  IconContent(this.icon, this.gender);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+          color: Color(0xFFFFFFFF),
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          gender,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
   final Color colour;
-  ReusableCard(this.colour);
+  final Widget cardChild;
+  ReusableCard(this.colour, this.cardChild);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: cardChild,
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         color: colour,
